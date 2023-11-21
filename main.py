98aa9ddb7f488d2722c8de8fd042eaaaf8937d39
boeto <bo@eto.me>
2023-11-21 22:33:55 +0800

    fix

--- main.py
+++ main.py
@@ -19,8 +19,7 @@ if __name__ == "__main__":
         print("配置文件有误，请检查")
         print(e)
     while True:
-        # url = input("请输入视频链接：")
-        url = "https://v.youku.com/v_show/id_XNjEyNzIxMDQ5Mg==.html?s=fffe6088749e457d9d67&spm=a2hje.13141534.1_3.d_1_1&scm=20140719.apircmd.240015.video_XNjEyNzIxMDQ5Mg=="
+        url = input("请输入视频链接：")
         # if "iqiyi.com" in url:
         #     iq.run(url)
         if "youku.com" in url:
